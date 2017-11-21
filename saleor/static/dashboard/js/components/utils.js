e336f7a06c190169c8d8543e37a7c74c2239fd5c
dominik-zeglen <flesz3@o2.pl>
2017-11-21 14:47:37 +0100

    Add href parameter to delete filter links

--- saleor/static/dashboard/js/components/utils.js
+++ saleor/static/dashboard/js/components/utils.js
@@ -1,6 +1,7 @@
 import 'select2';
+import queryString from 'query-string';
 
-var supportsPassive = false;
+let supportsPassive = false;
 try {
   let opts = Object.defineProperty({}, 'passive', {
     get: function () {
@@ -18,4 +19,14 @@ export function onScroll(func) {
 export function initSelects() {
   $('select:not(.browser-default):not([multiple])').material_select();
   $('select[multiple]:not(.browser-default)').select2({width: '100%'});
+}
+
+export function removeFromQuery(name, value) {
+  const urlParams = queryString.parse(location.search);
+  if (Array.isArray(urlParams[name])) {
+    delete urlParams[name][urlParams[name].indexOf(value)];
+  } else {
+    delete urlParams[name];
+  }
+  return queryString.stringify(urlParams);
 }
